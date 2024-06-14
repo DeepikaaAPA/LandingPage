e18293bfdcc00e23260e9033f0bf30f60bbcd249
DeepikaaAPA <deepikaarjun@gmail.com>
2024-06-14 23:11:13 +0530

    Add: Dynamic load features

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -4,42 +4,24 @@ export function Features() {
     <section className="features-icons bg-light text-center">
       <div className="container">
         <div className="row">
-          <div className="col-lg-4">
-            <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-              <div className="features-icons-icon d-flex">
-                <i className="bi-window m-auto text-primary"></i>
-              </div>
-              <h3>Fully Responsive</h3>
-              <p className="lead mb-0">
-                This theme will look great on any device, no matter the size!
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-              <div className="features-icons-icon d-flex">
-                <i className="bi-layers m-auto text-primary"></i>
-              </div>
-              <h3>Bootstrap 5 Ready</h3>
-              <p className="lead mb-0">
-                Featuring the latest build of the new Bootstrap 5 framework!
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="features-icons-item mx-auto mb-0 mb-lg-3">
-              <div className="features-icons-icon d-flex">
-                <i className="bi-terminal m-auto text-primary"></i>
-              </div>
-              <h3>Easy to Use</h3>
-              <p className="lead mb-0">
-                Ready to use with your own content, or customize the source
-                files!
-              </p>
-            </div>
-          </div>
+          {featuresList.map((feature) => (
+            <FeatureCard feature={feature} />
+          ))}
         </div>
       </div>
     </section>
+  );
+}
+function FeatureCard({ feature }) {
+  return (
+    <div className="col-lg-4">
+      <div className="features-icons-item mx-auto mb-0 mb-lg-3">
+        <div className="features-icons-icon d-flex">
+          <i className={feature.icon + " m-auto text-primary"}></i>
+        </div>
+        <h3>{feature.name}</h3>
+        <p className="lead mb-0">{feature.description.short}</p>
+      </div>
+    </div>
   );
 }
