374c943f6852492dfe82b64c4744f0764257580c
DeepikaaAPA <deepikaarjun@gmail.com>
2024-06-14 22:18:51 +0530

    upload:First Commit of files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <Features />
+      <Reviews />
+      <Subscribe />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
