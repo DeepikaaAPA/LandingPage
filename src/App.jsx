5c52d3c94a4c909f3e1f4bbf3b6bdcc5509500f7
DeepikaaAPA <deepikaarjun@gmail.com>
2024-06-15 10:01:55 +0530

    Remove : App.css Add: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { NavBar } from "./Components/NavBar";
 import { Header } from "./Components/Header";
 import { Features } from "./Components/Features";
-import "./App.css";
 import { Reviews } from "./Components/Reviews";
 import { CallToAction } from "./Components/CallToAction";
+import { Footer } from "./Components/Footer";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Features />
       <Reviews />
       <CallToAction />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
